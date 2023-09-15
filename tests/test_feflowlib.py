b90dc6e3de74e7b7a8a936f0cf971cf48095df19
Julian Heinze <julian.heinze@ufz.de>
2023-09-15 13:22:31 +0200

    fixed tests according to renaming

--- tests/test_feflowlib.py
+++ tests/test_feflowlib.py
@@ -8,11 +8,11 @@ import pyvista as pv
 pytest.importorskip("ifm")
 import ifm_contrib as ifm  # noqa: E402
 
-from ogstools.fe2vtu.feflowlib import get_pts_cells  # noqa: E402
+from ogstools.feflowlib.feflowlib import points_and_cells  # noqa: E402
 
 
 def test_cli():
-    subprocess.run(["fe2vtu", "--help"], check=True)
+    subprocess.run(["feflow2ogs", "--help"], check=True)
 
 
 current_dir = Path(__file__).parent
@@ -21,9 +21,9 @@ current_dir = Path(__file__).parent
 class TestConverter(unittest.TestCase):
     def test_converter(self):
         doc = ifm.loadDocument(
-            str(Path(current_dir / "data/fe2vtu/2layers_model.fem"))
+            str(Path(current_dir / "data/feflowlib/2layers_model.fem"))
         )
-        points, cells, celltypes = get_pts_cells(doc)
+        points, cells, celltypes = points_and_cells(doc)
         assert len(points) == 75
         assert len(celltypes) == 32
         assert celltypes[0] == pv.CellType.HEXAHEDRON
