58cef72b7a55edb34fe99ae31f3715f4404b0ef5
FZill <florian.zill@ufz.de>
2023-10-16 17:22:39 +0200

    refactor convergence code

--- ogstools/studies/convergence.py
+++ ogstools/studies/convergence.py
@@ -1,9 +1,10 @@
 from copy import deepcopy
+from typing import Union
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pyvista as pv
 
-from ogstools.meshlib import MeshSeries
 from ogstools.propertylib import (
     MatrixProperty,
     Property,
@@ -12,15 +13,18 @@ from ogstools.propertylib import (
 
 _element_length_key = "element_length_mean"
 
+# TODO: add grid convergence Index GCI
+# TODO: different properties based on the same data_name -> need other key
 
-def sampled_meshes(reference_mesh: pv.DataSet, meshes: list[pv.DataSet]):
-    sim_results_sampled = []
+
+def sampled_meshes(target_topology: pv.DataSet, meshes: list[pv.DataSet]):
+    meshes_resampled = []
     for mesh in meshes:
-        mesh_temp = deepcopy(reference_mesh)
+        mesh_temp = deepcopy(target_topology)
         mesh_temp = mesh_temp.sample(mesh, pass_cell_data=False)
-        sim_results_sampled += [mesh_temp]
+        meshes_resampled += [mesh_temp]
 
-    return sim_results_sampled
+    return meshes_resampled
 
 
 def element_length_mean(mesh: pv.DataSet):
@@ -33,107 +37,128 @@ def error(mesh: pv.DataSet, mesh_reference: pv.DataSet, property: Property):
         if isinstance(property, (VectorProperty, MatrixProperty))
         else property
     )
+
+    def _data(m: pv.DataSet):
+        return _p_val.values(m.point_data[property.data_name])
+
     return np.linalg.norm(
-        _p_val.values(mesh_reference.point_data[property.data_name])
-        - _p_val.values(mesh.point_data[property.data_name]),
-        axis=0,
-        ord=2,
-    ) / np.linalg.norm(
-        _p_val.values(mesh_reference.point_data[property.data_name]),
-        axis=0,
-        ord=2,
-    )
+        _data(mesh_reference) - _data(mesh), axis=0, ord=2
+    ) / np.linalg.norm(_data(mesh_reference), axis=0, ord=2)
 
 
-# TODO: different properties based on the same data_name -> need other key
-def plot_property(property: Property, convergence_data, ax=0):
+def plot_property(property: Property, convergence_data: dict, ax: plt.Axes):
     errors_per_mesh = convergence_data[property.data_name]
     e_lengths = convergence_data[_element_length_key]
     linear_refs_per_mesh = [
         (errors_per_mesh[0] / e_lengths[0]) * el**1 for el in e_lengths
-    ]  # to plot
+    ]
     quadratic_refs_per_mesh = [
         (errors_per_mesh[0] / e_lengths[0] ** 2) * el**2 for el in e_lengths
-    ]  # to plot
+    ]
 
     ax.loglog(e_lengths, errors_per_mesh, "-o")
     ax.loglog(e_lengths, linear_refs_per_mesh, "--", c="k")
     ax.loglog(e_lengths, quadratic_refs_per_mesh, ":", c="k")
-    # ax.set_title(title, loc="center", y=1.02)
     ax.set_xlabel("mean element length / m")
     ax.set_ylabel(f"{property.output_name} error / -")
-    ax.legend(["L2 Norm", "p=1", "p=2"])
+    ax.legend(["L2 Norm", "linear convergence", "quadratic convergence"])
     ax.grid(True, "major", "both", alpha=0.5)
     ax.grid(True, "minor", "both", alpha=0.1)
     return ax
 
 
 def plot_convergence(
-    refence_sim_result,
-    sim_result_files,
-    ts,
-    properties: list[Property],
-    axs,
+    target_topology: pv.DataSet,
+    meshes: list[pv.DataSet],
+    reference_mesh: pv.DataSet,
+    properties: Union[Property, list[Property]],
+    axs: Union[plt.Axes, list[plt.Axes]],
 ):
-    if not isinstance(axs, list):
+    """
+    Plot convergence data for multiple properties on different meshes.
+
+    This function plots convergence data for multiple properties of interest
+    calculated across different meshes in comparison to a reference mesh.
+    It provides visual representations of how the properties converge with
+    increasing mesh refinement.
+
+    For more information see :func:`convergence`.
+    """
+    if isinstance(axs, plt.Axes):
         axs = [axs]
-    metrics = convergence(refence_sim_result, sim_result_files, ts, properties)
-    for pos, property in enumerate(properties):
-        plot_property(property, metrics, axs[pos])
+    if not isinstance(properties, list):
+        properties = [properties]
+    metrics = convergence(target_topology, meshes, reference_mesh, properties)
+    for index, property in enumerate(properties):
+        plot_property(property, metrics, axs[index])
     return axs
 
 
-# TODO: add grid convergence Index GCI
-
-
 def richardson_extrapolation(
-    mesh1: pv.DataSet, mesh2: pv.DataSet, property: Property, r: float
+    mesh1: pv.DataSet,
+    mesh2: pv.DataSet,
+    properties: Union[Property, list[Property]],
 ):
     """
-    Estimate a more accurate approximation of a property on a mesh.
+    Estimate a more accurate approximation of properties on a mesh.
 
-    This function assumes that both mesh1 and mesh2 are compatible, meaning they
-    share the same structure and topology, but their data should correspond to
-    different discretizations, suggestively sampled on the base topology.
+    This function calculates the Richardson Extrapolation based on the change
+    in the discretization and the results of the two given meshes.
+    The result is given on the topology of mesh1.
     See <https://www.grc.nasa.gov/www/wind/valid/tutorial/spatconv.html> for
-    more info on the topic.
+    more information on this topic.
 
-    :param mesh1:       The mesh with data from coarser discretization.
-    :param mesh2:       The mesh with data from finer discretization.
-    :param property:    The property to be extrapolated.
-    :param r:           The refinement ratio of the underlying discretizations.
+    :param mesh1:       The mesh with the coarser discretization.
+    :param mesh2:       The mesh with the finer discretization.
+    :param properties:  The properties to be extrapolated.
 
-    :returns:           A new DataSet with the extrapolated property values
+    :returns:           The topology of mesh1 with the properties values
                         based on Richardson Extrapolation.
     """
     rich_ex = deepcopy(mesh1)
-    f1 = mesh1.point_data[property.data_name]
-    f2 = mesh2.point_data[property.data_name]
-    rich_ex.point_data[property.data_name] = f2 + (f1 - f2) / (r * r - 1)
+    r = element_length_mean(mesh1) / element_length_mean(mesh2)
+    if not isinstance(properties, list):
+        properties = [properties]
+    for property in properties:
+        f1 = mesh1.point_data[property.data_name]
+        f2 = mesh1.sample(mesh2).point_data[property.data_name]
+        rich_ex.point_data[property.data_name] = f2 + (f1 - f2) / (r * r - 1)
     return rich_ex
 
 
 def convergence(
-    sim_result_reference, sim_result_files, ts, properties: list[Property]
+    target_topology: pv.DataSet,
+    meshes: list[pv.DataSet],
+    reference_mesh: pv.DataSet,
+    properties: Union[Property, list[Property]],
 ):
-    reference_mesh = MeshSeries(sim_result_reference).read(ts)
-    meshes = [
-        MeshSeries(sim_result).read(ts) for sim_result in sim_result_files
-    ]
-    meshes_sampled = sampled_meshes(reference_mesh, meshes)
+    """
+    Compute the convergence of numerical simulations on multiple meshes.
 
-    # dictionary suitable for direct converting to pandas
-    # pandas.DataFrame(d)
+    The convergence is assessed for multiple properties of interest by comparing
+    the results on various meshes to a common reference mesh.
 
+    :param target_topology: The mesh providing the topology for result sampling.
+    :param meshes:          A list of meshes which get evaluated.
+    :param reference_mesh:  The reference mesh to compare against.
+    :param properties:      A list of properties to be compared for convergence.
+
+    :returns: A dictionary containing convergence information for each property.
+              The keys are the property names, and the values are lists of
+              convergence values for each mesh.
+
+              Convergence is calculated based on the error between the results
+              obtained on each mesh and the reference mesh, normalized by the
+              mean element length of the target topology mesh.
+    """
+    meshes_sampled = sampled_meshes(target_topology, meshes + [reference_mesh])
     mean_lengts = [element_length_mean(mesh) for mesh in meshes]
     convergence_dict = {_element_length_key: mean_lengts}
-    ref_ratio = mean_lengts[-2] / mean_lengts[-1]
+    if not isinstance(properties, list):
+        properties = [properties]
     for property in properties:
-        rich_ex = richardson_extrapolation(
-            meshes_sampled[-2], meshes_sampled[-1], property, ref_ratio
-        )
         convergence_dict[property.data_name] = [
-            error(mesh_sampled, rich_ex, property)
-            for mesh_sampled in meshes_sampled
+            error(mesh_sampled, meshes_sampled[-1], property)
+            for mesh_sampled in meshes_sampled[:-1]
         ]
     return convergence_dict
