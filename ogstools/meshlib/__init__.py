053440e0272e85d2bddd37a4df54b204b476713b
FZill <florian.zill@ufz.de>
2023-11-09 16:04:53 +0100

    msh2vtu use logging for output
    moved some gmsh scripts to meshlib

--- ogstools/meshlib/__init__.py
+++ ogstools/meshlib/__init__.py
@@ -1,6 +1,7 @@
 from .boundary import Boundary, Layer, LocationFrame, Raster
 from .boundary_set import LayerSet
 from .boundary_subset import Surface
+from .gmsh_meshing import cuboid_mesh, rect_mesh
 from .mesh_series import MeshSeries
 
 __all__ = [
@@ -11,4 +12,6 @@ __all__ = [
     "Boundary",
     "MeshSeries",
     "LayerSet",
+    "rect_mesh",
+    "cuboid_mesh",
 ]
