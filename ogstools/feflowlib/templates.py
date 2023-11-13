be14f8acb75edbc53980e4fd08e4dca32b99fb2c
Julian Heinze <julian.heinze@ufz.de>
2023-11-13 13:35:16 +0100

    add template for LiquidFlow and add petsc lin_solver

--- ogstools/feflowlib/templates.py
+++ ogstools/feflowlib/templates.py
@@ -51,4 +51,84 @@ def steady_state_diffusion(saving_path, model=None):
         max_iteration_step="100000",
         error_tolerance="1e-6",
     )
+    model.linsolvers.add_lin_solver(
+        name="general_linear_solver",
+        kind="petsc",
+        prefix="sd",
+        solver_type="cg",
+        precon_type="bjacobi",
+        max_iteration_step="10000",
+        error_tolerance="1e-16",
+    )
+    return model
+
+
+def liquid_flow(saving_path, model=None):
+    """
+    A template for steady state diffusion process to be simulated in ogs.
+    """
+    model.processes.set_process(
+        name="LiquidFlow",
+        type="LIQUID_FLOW",
+        integration_order="2",
+        specific_body_force="0 0 0",
+        linear="true",
+    )
+    model.processes.add_process_variable(
+        secondary_variable="darcy_velocity", output_name="v"
+    )
+    model.timeloop.add_process(
+        process="LiquidFlow",
+        nonlinear_solver_name="basic_picard",
+        convergence_type="DeltaX",
+        norm_type="NORM2",
+        abstol="1e-10",
+        time_discretization="BackwardEuler",
+    )
+    model.timeloop.set_stepping(
+        process="LiquidFlow",
+        type="FixedTimeStepping",
+        t_initial="0",
+        t_end="1",
+        repeat="1",
+        delta_t="1",
+    )
+    model.timeloop.add_output(
+        type="VTK",
+        prefix=saving_path,
+        repeat="1",
+        each_steps="1",
+        variables=[],
+    )
+    model.nonlinsolvers.add_non_lin_solver(
+        name="basic_picard",
+        type="Picard",
+        max_iter="10",
+        linear_solver="general_linear_solver",
+    )
+    model.linsolvers.add_lin_solver(
+        name="general_linear_solver",
+        kind="lis",
+        solver_type="cg",
+        precon_type="jacobi",
+        max_iteration_step="10000",
+        error_tolerance="1e-20",
+    )
+    model.linsolvers.add_lin_solver(
+        name="general_linear_solver",
+        kind="eigen",
+        solver_type="CG",
+        precon_type="DIAGONAL",
+        max_iteration_step="100000",
+        error_tolerance="1e-20",
+    )
+    model.linsolvers.add_lin_solver(
+        name="general_linear_solver",
+        kind="petsc",
+        prefix="lf",
+        solver_type="cg",
+        precon_type="bjacobi",
+        max_iteration_step="10000",
+        error_tolerance="1e-16",
+    )
     return model
