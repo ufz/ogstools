Bug fix in layer computation

ogstools/meshlib/gmsh_meshing.py
@@ -189,10 +189,7 @@ def gen_bhe_mesh_gmsh(
                     - space_next_layer_refined
                 )
                 if space_next_layer_refined == 0:
-                    if (
-                        space
-                        <= (n_refinement_layers + 1) * target_z_size_coarse
-                    ):
+                    if space <= target_z_size_fine:
                         absolute_height_of_layers.append(
                             np.abs(z_min - z_max)
                         )  # space
@@ -208,20 +205,12 @@ def gen_bhe_mesh_gmsh(
                         number_of_layers[len(number_of_layers) - 1].append(
                             n_refinement_layers
                         )
-                        absolute_height_of_layers.append(
-                            space - n_refinement_layers * target_z_size_fine
-                        )
+                        absolute_height_of_layers.append(space)
                         number_of_layers[len(number_of_layers) - 1].append(
-                            math.ceil(
-                                (
-                                    space
-                                    - n_refinement_layers * target_z_size_fine
-                                )
-                                / target_z_size_coarse
-                            )
+                            math.ceil((space) / target_z_size_coarse)
                         )
                 else:
-                    if space <= target_z_size_coarse:
+                    if space <= target_z_size_fine:
                         absolute_height_of_layers.append(
                             space
                             + n_refinement_layers * target_z_size_fine
@@ -1485,12 +1474,14 @@ def gen_bhe_mesh_gmsh(
                 start_groundwater = i
                 # needed_extrusions=len(layer)+1
                 # icl=0
+                """
                 if (
                     np.abs(groundwater[g][0])
                     < n_refinement_layers * target_z_size_fine
                 ):  # pragma: no cover
                     msg = "Groundwater layer must start in the soil, a beginning in the first 2 meter of the top surface is currently not possible!"
                     raise Exception(msg)
+                """
                 if (  # previous elif, one semantic block of different cases -> switch to if, because of ruff error
                     np.abs(groundwater[g][0])
                     - np.sum(layer[:start_groundwater])