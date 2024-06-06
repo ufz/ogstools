ed02107e9f91fcf654282725bd16f9b0365fa6a1
Tobias Meisel <tobias.meisel@ufz.de>
2024-06-06 11:56:06 +0200

    Fix ruff in feflowlib

--- ogstools/feflowlib/_feflowlib.py
+++ ogstools/feflowlib/_feflowlib.py
@@ -83,7 +83,7 @@ def points_and_cells(doc: ifm.FeflowDoc) -> tuple[np.ndarray, list, list]:
 
 def _material_ids_from_selections(
     doc: ifm.FeflowDoc,
-) -> dict:
+) -> np.ndarray:
     """
     Get MaterialIDs from the FEFLOW data. Only applicable if they are
     saved in doc.c.sel.selections().
@@ -130,7 +130,7 @@ def _material_ids_from_selections(
     # 4. log the dictionary of the MaterialIDs
     logger.info("MaterialIDs refer to: %s", dict_matid)
     # MaterialIDs must be int32
-    return {"MaterialIDs": np.array(mat_ids_mesh).astype(np.int32)}
+    return  np.array(mat_ids_mesh).astype(np.int32)
 
 
 def fetch_user_data(
@@ -198,9 +198,7 @@ def _point_and_cell_data(
     cell_data = {key: [cell_data[key]] for key in cell_data}
 
     # 6. add MaterialIDs to cell data
-    cell_data[str(list(MaterialIDs.keys())[0])] = [
-        list(MaterialIDs.values())[0]
-    ]
+    cell_data['MaterialIDs'] = list(MaterialIDs)
 
     # if P_LOOKUP_REGION is given and there are more different MaterialIDs given
     # than defined in selections, use P_LOOKUP_REGION for MaterialIDs
