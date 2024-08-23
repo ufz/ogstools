e16323eb2b0445b841a78236bdb7d190e89078b0
joergbuchwald <joerg.buchwald@ufz.de>
2024-08-23 12:13:00 +0200

    bugfix release 0.403

--- ogstools/ogs6py/__init__.py
+++ ogstools/ogs6py/__init__.py
@@ -7,21 +7,5 @@
 # Author: Joerg Buchwald (Helmholtz Centre for Environmental Research GmbH - UFZ)
 
 from .ogs import OGS
-from .build_tree import BuildTree
-from .curves import Curves
-from .geo import Geo
-from .linsolvers import LinSolvers
-from .local_coordinate_system import LocalCoordinateSystem
-from .media import Media
-from .mesh import Mesh
-from .nonlinsolvers import NonLinSolvers
-from .parameters import Parameters
-from .processes import Processes
-from .processvars import ProcessVars
-from . import properties
-from .python_script import PythonScript
-from .timeloop import TimeLoop
 
-__all__ = [
-    "OGS"
-]
+__all__ = ["OGS"]
