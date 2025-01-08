46af2ea4357ad621cbefd0de8662dbd622aa0aea
FZill <florian.zill@ufz.de>
2025-01-08 09:40:43 +0100

    remove try except block in top level import
    confusing when import works, but somewhat arbitrarily some packages are not available

--- ogstools/__init__.py
+++ ogstools/__init__.py
@@ -8,18 +8,14 @@
 
 from importlib import metadata
 
+from . import plot, variables
+from .meshlib import Mesh, MeshSeries  # noqa: F401
+from .ogs6py import Project  # noqa: F401
+
 __version__ = metadata.version(__package__)
 __authors__ = metadata.metadata(__package__)["Author-email"]
 
 del metadata  # optional, avoids polluting the results of dir(__package__)
-
-try:
-    from . import plot, variables
-    from .meshlib import Mesh, MeshSeries  # noqa: F401
-    from .ogs6py import Project  # noqa: F401
-except ImportError as e:
-    print(f"Warning: Failed to import the following libraries: {e.name}")
-
 
 __all__ = [
     "plot",
