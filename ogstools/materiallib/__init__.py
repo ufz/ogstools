610983bbfcec02d311ac190c06a83fc57889e138
Norbert Grunwald <norbert.grunwald@ufz.de>
2025-03-21 10:38:19 +0100

    remove custom yml loader

--- ogstools/materiallib/__init__.py
+++ ogstools/materiallib/__init__.py
@@ -1,5 +1,6 @@
 # ogstools/materiallib/__init__.py
-from .io.yaml_loader import load_all_yaml
+import yaml
+from pathlib import Path
 
 class MaterialLib:
     def __init__(self):
@@ -7,10 +8,19 @@ class MaterialLib:
         self._load_materials()
 
     def _load_materials(self):
-        yaml_data = load_all_yaml()
-        for filename, data in yaml_data.items():
-            material_name = data.get("name", filename)
-            self.materials[material_name] = data
+        data_dir = Path(__file__).parent.parent / "data"
+        yaml_files = list(data_dir.glob("*.yml")) + list(data_dir.glob("*.yaml"))
+
+        if not yaml_files:
+            raise FileNotFoundError(f"No YAML files found in the data directory ({data_dir}).")
+
+        self.materials = {}
+        for file_path in yaml_files:
+            with open(file_path, "r", encoding="utf-8") as file:
+                data = yaml.safe_load(file)
+                material_name = data.get("name", file_path.stem)
+                print(f"MAterial found: {material_name}")
+                self.materials[material_name] = data
 
     def list_materials(self):
         return list(self.materials.keys())
