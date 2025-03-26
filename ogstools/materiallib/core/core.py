3ea0818f0ad7af745356232d799410ce7bb1f83b
Norbert Grunwald <norbert.grunwald@ufz.de>
2025-03-26 11:26:49 +0100

    [MatLib] new Poperty class

--- ogstools/materiallib/core/core.py
+++ ogstools/materiallib/core/core.py
@@ -3,13 +3,14 @@ import yaml
 
 
 class Property:
-    def __init__(self, name: str, type_: str, value: float | None = None, **extra):
+    def __init__(self, name: str, type_: str, value=None, **extra):
+    #def __init__(self, name: str, type_: str, value: float | None = None, **extra):
         self.name = name
         self.type = type_
         self.value = value
-        self.extra = extra  # weitere Keys wie slope, unit, etc.
+        self.extra = extra  # e.g. unit, slope, source, ...
 
-    def to_ogs_dict(self) -> dict:
+    def to_dict(self):
         d = {"name": self.name, "type": self.type}
         if self.value is not None:
             d["value"] = self.value
