a53f59732a6c3b9a3c461dcf32edd839a9d87041
Norbert Grunwald <norbert.grunwald@ufz.de>
2025-03-27 14:23:17 +0100

    new structure for process_schema structure

--- ogstools/materiallib/schema/process_schema.py
+++ ogstools/materiallib/schema/process_schema.py
@@ -1,82 +1,142 @@
 PROCESS_SCHEMAS = {
-    "HeatConduction": {
-        "thermal_conductivity": "medium",
-        "density": "medium",
-        "specific_heat_capacity": "medium",
-        "_fluids": {},
-    },
-    "TH2M": {
-        # Solid phase
-        "density": "solid",
-        "specific_heat_capacity": "solid",
-        "thermal_conductivity": "solid",
-        "thermal_expansivity": "solid",
-        # Medium properties
-        "porosity": "medium",
-        "permeability": "medium",
-        "saturation": "medium",
-        "bishops_effective_stress": "medium",
-        # Fluid properties (without phase transition)
-        "_fluids": {
-            "AqueousLiquid": {
-                "phase_properties": [
-                    "density",
-                    "viscosity",
-                    "thermal_conductivity",
-                    "specific_heat_capacity",
-                ]
-            },
-            "Gas": {
-                "phase_properties": [
-                    "density",
-                    "viscosity",
-                    "thermal_conductivity",
-                ]
-            },
-        },
-    },
     "TH2M_PT": {
-        # Solid phase
-        "density": "solid",
-        "specific_heat_capacity": "solid",
-        "thermal_conductivity": "solid",
-        "thermal_expansivity": "solid",
-        # Medium properties
-        "porosity": "medium",
-        "permeability": "medium",
-        "saturation": "medium",
-        "bishops_effective_stress": "medium",
-        # Fluid properties with phase transitions - component properties
-        "_fluids": {
-            "AqueousLiquid": {
-                "component_properties": [
-                    "molar_mass",
-                    "specific_heat_capacity",
-                    "diffusion",
-                    "henry_coefficient",
-                    "specific_latent_heat",
-                ],
-                "phase_properties": [
-                    "density",
-                    "viscosity",
+        "phases": [
+            {
+                "type": "AqueousLiquid",
+                "components": {
+                    "A": [
+                        "specific_heat_capacity",
+                        "henry_coefficient",
+                        "diffusion",
+                        "specific_latent_heat",
+                    ],
+                    "W": ["specific_heat_capacity"],
+                },
+                "properties": [
                     "thermal_conductivity",
                     "specific_heat_capacity",
-                ],
-            },
-            "Gas": {
-                "component_properties": [
-                    "molar_mass",
-                    "specific_heat_capacity",
-                    "specific_latent_heat",
-                    "vapour_pressure",
-                    "diffusion",
-                ],
-                "phase_properties": [
                     "density",
                     "viscosity",
-                    "thermal_conductivity",
                 ],
             },
-        },
-    },
+            {
+                "type": "Gas",
+                "components": {
+                    "A": ["molar_mass", "specific_heat_capacity"],
+                    "W": [
+                        "molar_mass",
+                        "specific_heat_capacity",
+                        "specific_latent_heat",
+                        "vapour_pressure",
+                        "diffusion",
+                    ],
+                },
+                "properties": ["thermal_conductivity", "density", "viscosity"],
+            },
+            {
+                "type": "Solid",
+                "properties": [
+                    "density",
+                    "thermal_conductivity",
+                    "specific_heat_capacity",
+                    "thermal_expansivity",
+                ],
+            },
+        ],
+        "properties": [
+            "permeability",
+            "biot_coefficient",
+            "saturation",
+            "relative_permeability_nonwetting_phase",
+            "relative_permeability",
+            "porosity",
+            "tortuosity",
+            "thermal_conductivity",
+            "bishops_effective_stress",
+        ],
+    }
 }
+
+
+# PROCESS_SCHEMAS = {
+#     "HeatConduction": {
+#         "thermal_conductivity": "medium",
+#         "density": "medium",
+#         "specific_heat_capacity": "medium",
+#         "_fluids": {},
+#     },
+#     "TH2M": {
+#         # Solid phase
+#         "density": "solid",
+#         "specific_heat_capacity": "solid",
+#         "thermal_conductivity": "solid",
+#         "thermal_expansivity": "solid",
+#         # Medium properties
+#         "porosity": "medium",
+#         "permeability": "medium",
+#         "saturation": "medium",
+#         "bishops_effective_stress": "medium",
+#         # Fluid properties (without phase transition)
+#         "_fluids": {
+#             "AqueousLiquid": {
+#                 "phase_properties": [
+#                     "density",
+#                     "viscosity",
+#                     "thermal_conductivity",
+#                     "specific_heat_capacity",
+#                 ]
+#             },
+#             "Gas": {
+#                 "phase_properties": [
+#                     "density",
+#                     "viscosity",
+#                     "thermal_conductivity",
+#                 ]
+#             },
+#         },
+#     },
+#     "TH2M_PT": {
+#         # Solid phase
+#         "density": "solid",
+#         "specific_heat_capacity": "solid",
+#         "thermal_conductivity": "solid",
+#         "thermal_expansivity": "solid",
+#         # Medium properties
+#         "porosity": "medium",
+#         "permeability": "medium",
+#         "saturation": "medium",
+#         "bishops_effective_stress": "medium",
+#         # Fluid properties with phase transitions - component properties
+#         "_fluids": {
+#             "AqueousLiquid": {
+#                 "component_properties": [
+#                     "molar_mass",
+#                     "specific_heat_capacity",
+#                     "diffusion",
+#                     "henry_coefficient",
+#                     "specific_latent_heat",
+#                 ],
+#                 "phase_properties": [
+#                     "density",
+#                     "viscosity",
+#                     "thermal_conductivity",
+#                     "specific_heat_capacity",
+#                 ],
+#             },
+#             "Gas": {
+#                 "component_properties": [
+#                     "molar_mass",
+#                     "specific_heat_capacity",
+#                     "specific_latent_heat",
+#                     "vapour_pressure",
+#                     "diffusion",
+#                 ],
+#                 "phase_properties": [
+#                     "density",
+#                     "viscosity",
+#                     "thermal_conductivity",
+#                 ],
+#             },
+#         },
+#     },
+# }
