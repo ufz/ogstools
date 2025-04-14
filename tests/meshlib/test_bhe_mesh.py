c79e6051e5b4597347c22565f124c9ef16d18624
Max Jaeschke <max.jaeschke@htwk-leipzig.de>
2025-04-14 07:56:20 +0200

    Add test for large BHE array

--- tests/meshlib/test_bhe_mesh.py
+++ tests/meshlib/test_bhe_mesh.py
@@ -1,3 +1,4 @@
+from itertools import product
 from pathlib import Path
 from tempfile import mkdtemp
 
@@ -70,13 +71,44 @@ def case_4(vtu_out_file_path: Path, mesh_type: str) -> list[str]:
     )
 
 
+def case_5(vtu_out_file_path: Path, mesh_type: str) -> list[str]:
+    # test a 12x12 bhe array
+    n_bhe_per_row = 12
+    dist_bhe = 2.5
+    border_distance = 2.5 * dist_bhe
+    spacing_bhe = (
+        np.linspace(0, (n_bhe_per_row - 1) * dist_bhe, n_bhe_per_row)
+        + border_distance
+    )
+
+    bhe_array = [
+        BHE(bhe_x, bhe_y, 0, -10, 0.076)
+        for bhe_x, bhe_y in product(spacing_bhe, repeat=2)
+    ]
+    return gen_bhe_mesh(
+        length=np.max(spacing_bhe) + 2 * border_distance,
+        width=np.max(spacing_bhe) + 2 * border_distance,
+        layer=[15],
+        groundwater=[],
+        BHE_Array=bhe_array,
+        meshing_type=mesh_type,
+        out_name=vtu_out_file_path,
+        inner_mesh_size=10,
+        outer_mesh_size=20,
+        dist_box_x=4,
+        dist_box_y=4,
+        n_refinement_layers=0,
+        target_z_size_coarse=10,
+    )
+
+
 class TestBHE:
     tmp_dir = Path(mkdtemp())
 
     @parameterized.expand(((0, 4), (1, 5), (2, 7), (3, 7)))
     def test_bhe_mesh_structured(self, index: int, max_id: int):
         vtu_file = self.tmp_dir / f"bhe_structured_{index}.vtu"
-        model = [case_1, case_2, case_3, case_4][index]
+        model = [case_1, case_2, case_3, case_4, case_5][index]
         meshes = model(vtu_out_file_path=vtu_file, mesh_type="structured")
         for mesh in meshes:
             assert Path(self.tmp_dir / mesh).is_file()
@@ -88,10 +120,10 @@ class TestBHE:
         )
         assert np.isin(bhe_line.points, soil.points).all()
 
-    @parameterized.expand(((0, 4), (1, 5), (2, 7)))
+    @parameterized.expand(((0, 4), (1, 5), (2, 7), (3, 144)))
     def test_bhe_mesh_prism(self, index: int, max_id: int):
         vtu_file = self.tmp_dir / f"bhe_prism_{index}.vtu"
-        model = [case_1, case_2, case_3][index]
+        model = [case_1, case_2, case_3, case_5][index]
         meshes = model(vtu_out_file_path=vtu_file, mesh_type="prism")
         for mesh in meshes:
             assert Path(self.tmp_dir / mesh).is_file()
