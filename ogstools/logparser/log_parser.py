b8a127bf36301d6e4e583183c459d22a812692f3
Tobias Meisel <tobias.meisel@ufz.de>
2025-06-11 10:27:36 +0200

    LogParser: Remove duplicate copyright

--- ogstools/logparser/log_parser.py
+++ ogstools/logparser/log_parser.py
@@ -4,11 +4,6 @@
 #            http://www.opengeosys.org/project/license
 #
 
-# Copyright (c) 2012-2025, OpenGeoSys Community (http://www.opengeosys.org)
-#            Distributed under a Modified BSD License.
-#              See accompanying file LICENSE.txt or
-#              http://www.opengeosys.org/project/license
-
 import re
 from dataclasses import asdict
 from pathlib import Path
