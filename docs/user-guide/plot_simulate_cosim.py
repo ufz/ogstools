12a74aed251c2648f6a062e45f121f02a80a4e0b
Tobias Meisel <tobias.meisel@ufz.de>
2025-08-19 12:23:43 +0200

    Simulation Co-sim docu

--- docs/user-guide/plot_simulate_cosim.py
+++ docs/user-guide/plot_simulate_cosim.py
@@ -1,80 +1,110 @@
-import pyvista as pv
-import numpy as np
+"""
+Co-Simulation
+=============
 
+ToDo Explanation
+
+"""
+
+# %%
+# Imports and definitions
 from pathlib import Path
 from tempfile import mkdtemp
 
+import numpy as np
+import pyvista as pv
+from ogs import simulator
+
 import ogstools as ot
 from ogstools.definitions import EXAMPLES_DIR
-from ogs import mesh, simulator
 
 results_path = Path(mkdtemp())
 prj_path = EXAMPLES_DIR / "prj" / "SimpleLF.prj"
 gmsh_mesh_name = results_path / "rect.msh"
 
-def generateModelMeshes():
-    ot.meshlib.rect(
-        lengths=(10, 2),
-        n_edge_cells=(10, 4),
-        n_layers=2,
-        structured_grid=True,
-        order=1,
-        mixed_elements=False,
-        jiggle=0.0,
-        out_name=Path(gmsh_mesh_name),
-    )
 
-    meshes = ot.meshes_from_gmsh(gmsh_mesh_name)
-    points_shape = np.shape(meshes["physical_group_left"].points)
-    meshes["physical_group_left"].point_data["pressure"] = np.full(
-        points_shape[0], 2.99e7
-    )
-    meshes["physical_group_right"].point_data["pressure"] = np.full(
-        points_shape[0], 3e7
-    )
+# %%
+# Create mesh and boundary meshes
+# boundary meshes get initial pressure value
 
-    for name, mesh in meshes.items():
-        pv.save_meshio(Path(results_path, name + ".vtu"), mesh)
+ot.meshlib.rect(
+    lengths=(10, 2),
+    n_edge_cells=(10, 4),
+    n_layers=2,
+    structured_grid=True,
+    order=1,
+    mixed_elements=False,
+    jiggle=0.0,
+    out_name=Path(gmsh_mesh_name),
+)
 
+meshes = ot.meshes_from_gmsh(gmsh_mesh_name)
 
-def executeSimulation():
-    arguments = [
-        "",
-        str(prj_path),
-        "-l debug",
-        "-m",
-        str(results_path),
-        "-o",
-        str(results_path),
-    ]
-    simulator.initialize(arguments)
+# Adapt the boundary meshes
+points_shape = np.shape(meshes["physical_group_left"].points)
+meshes["physical_group_left"].point_data["pressure"] = np.full(
+    points_shape[0], 2.99e7
+)
+meshes["physical_group_right"].point_data["pressure"] = np.full(
+    points_shape[0], 3e7
+)
 
-    left_boundary = simulator.getMesh("physical_group_left")
-    pressure = np.array(left_boundary.getPointDataArray("pressure", 1))
-
-    # todo extract number of timesteps from project file
-    # simulator.executeSimulation()
-    for i in range(0, 15):
-        # modify left boundary condition values
-        if i < 10:
-            pressure = np.full(pressure.shape, 2.99e7)
-        else:
-            pressure = np.full(pressure.shape, 3.01e7)
-        left_boundary.setCellDataArray("pressure", pressure, 1)
-        simulator.executeTimeStep()
-
-    simulator.finalize()
+for name, mesh in meshes.items():
+    pv.save_meshio(Path(results_path, name + ".vtu"), mesh)
 
 
-def cleanupModelMeshes():
-    print("todo: rm created meshes")
+# %%
+# Initialize the simulation
+# Use same arguments as when calling ogs from command line, --> link
+
+arguments = [
+    "",
+    str(prj_path),
+    "-l debug",  # optional
+    "-m",  # optional, if mesh is located on different folder then prj file
+    str(results_path),
+    "-o",
+    str(results_path),
+]
+
+simulator.initialize(arguments)
+
+# %%
+# Get access to the current state of the mesh (here left boundary mesh)
+# link to getMesh
+
+left_boundary = simulator.getMesh("physical_group_left")
+pressure = np.array(left_boundary.getPointDataArray("pressure", 1))
 
 
-def main():
-    generateModelMeshes()
-    executeSimulation()
-    cleanupModelMeshes()
+# plot pressure
+
+# todo extract number of timesteps from project file
 
 
-if __name__ == "__main__":
-    main()
+# %%
+# Replace property values of the mesh with setCellDataArray or setPointDataArray
+#
+
+for i in range(15):
+    # modify left boundary condition values
+    if i < 10:
+        pressure = np.full(pressure.shape, 2.99e7)
+    else:
+        pressure = np.full(pressure.shape, 3.01e7)
+
+    # or setPointDataArray for node-centred properties
+    left_boundary.setCellDataArray("pressure", pressure, 1)
+
+    # Perform a single step (step size is determined by setting in projectfile/timeloop)
+    # In this example we have fixed step
+    simulator.executeTimeStep()
+
+    ## ToDo getTimeStep?
+
+
+# %%
+
+# Necessary to close, otherwise you can not reinitialize simulation with same prj-file (arguments)
+
+simulator.finalize()
