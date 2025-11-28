953b7a166dee3471a25266521c5305b211bcac02
FZill <florian.zill@ufz.de>
2025-11-28 07:29:46 +0100

    use Meshes.plot in example

--- docs/examples/howto_preprocessing/plot_extract_boundaries.py
+++ docs/examples/howto_preprocessing/plot_extract_boundaries.py
@@ -25,8 +25,7 @@ domain = examples.load_meshseries_THM_2D_PVD()[0]
 
 # %%
 meshes = ot.Meshes.from_mesh(domain)
-boundaries = meshes.subdomains
-for name, mesh in boundaries.items():
+for name, mesh in meshes.subdomains.items():
     print(name, mesh)
 
 
@@ -34,10 +33,7 @@ for name, mesh in boundaries.items():
 # Let's display and save them:
 
 # %%
-fig = ot.plot.contourf(domain, ot.variables.material_id)
-colors = ["black", "grey", "lime", "yellow"]
-for i, (name, mesh) in enumerate(boundaries.items()):
-    ot.plot.line(mesh, ax=fig.axes[0], lw=2, annotate=name, color=colors[i])
+fig = meshes.plot()
 
 # %%
 meshes.save()  # optional provide a path
