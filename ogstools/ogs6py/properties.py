7279823726102fcf1470b6b419798caeca2b304f
Tobias Meisel <tobias.meisel@ufz.de>
2026-02-05 18:59:30 +0100

    Handle python script

--- ogstools/ogs6py/properties.py
+++ ogstools/ogs6py/properties.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from ogstools.ogs6py import Project
+    from ogstools import Project
 
 from collections.abc import Generator
 from dataclasses import dataclass, field
