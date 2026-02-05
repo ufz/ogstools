15abdefd814f34b5192cdfcb1899525de276ac14
Tobias Meisel <tobias.meisel@ufz.de>
2026-02-05 16:36:10 +0100

    Add new class to top level API

--- ogstools/__init__.py
+++ ogstools/__init__.py
@@ -7,13 +7,24 @@
 from contextlib import suppress
 from importlib import metadata
 
+# Top API
 from . import gmsh_tools, logparser, mesh, plot, variables
-from ._find_ogs import cli, status
+from ._find_ogs import cli, status  # noqa: F401
+from .core.execution import Execution
+from .core.interactive_simulation_controller import (
+    OGSInteractiveController,  # noqa: F401
+)
+from .core.model import Model
+from .core.native_simulation_controller import OGSNativeController  # noqa: F401
+from .core.result import Result  # noqa: F401
+from .core.simulation import Simulation
+from .core.storage import StorageBase  # noqa: F401
+from .logparser.log import Log
 from .materiallib.core.material_manager import MaterialManager
 from .materiallib.core.media import MediaSet
-from .meshes import Meshes
-from .meshseries import MeshSeries
-from .ogs6py import Project
+from .meshes._meshes import Meshes
+from .meshseries._meshseries import MeshSeries
+from .ogs6py.project import Project
 
 with suppress(ImportError):
     from .feflowlib import FeflowModel  # noqa: F401
@@ -27,16 +38,18 @@ del metadata  # optional, avoids polluting the results of dir(__package__)
 """.. noindex::"""
 
 __all__ = [
+    "Execution",
+    "Log",
     "MaterialManager",
     "MediaSet",
     "MeshSeries",
     "Meshes",
+    "Model",
     "Project",
-    "cli",
+    "Simulation",
     "gmsh_tools",
     "logparser",
     "mesh",
     "plot",
-    "status",
     "variables",
 ]
