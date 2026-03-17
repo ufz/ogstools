646568a40a359e6d59b128661b9644622d515468
Tobias Meisel <tobias.meisel@ufz.de>
2026-03-17 11:14:36 +0100

    CLI() - Fix underscore for all tools
    some tools have - some _. For python we need _.
    we do automatic conversion from - to _. But the other way around we need the original
    definition from the tool

--- ogstools/_cli/wrap_cli_tools.py
+++ ogstools/_cli/wrap_cli_tools.py
@@ -5,9 +5,9 @@
 #
 
 import subprocess
-from collections.abc import Callable
+from collections.abc import Callable, Collection
 from pathlib import Path
-from typing import Any
+from typing import Any, ClassVar
 
 from .provide_ogs_cli_tools_via_wheel import binaries_list, ogs_with_args
 
@@ -54,8 +54,55 @@ class CLI_ON_PATH:
 
         return ogs_with_args(cmdline)
 
+    #  Tools that already use underscores in the CLI
+    # (e.g. --geometry_name is really the argument).
+    _UNDERSCORE_ARGS: ClassVar[dict[str, set[str]]] = {
+        "addDataToRaster": {
+            "ll_x",
+            "ll_y",
+            "ur_x",
+            "ur_y",
+            "offset_value",
+            "scaling_value",
+            "output_raster",
+        },
+        "AddElementQuality": {
+            "input_mesh_file",
+            "output_mesh_file",
+            "quality_criterion",
+        },
+        "checkMesh": {"print_properties"},
+        "createLayeredMeshFromRasters": {"ascii_output"},
+        "createRaster": {"cell_size", "ll_x", "ll_y", "n_cols", "n_rows"},
+        "createTetgenSmeshFromRasters": {"ascii_output"},
+        "generateGeometry": {"geometry_name", "polyline_name"},
+        "geometryToGmshGeo": {
+            "average_point_density",
+            "max_points_in_quadtree_leaf",
+            "mesh_density_scaling_at_points",
+            "mesh_density_scaling_at_stations",
+            "write_merged_geometries",
+        },
+        "identifySubdomains": {"output_prefix"},
+        "mergeMeshToBulkMesh": {
+            "capillary_pressure",
+            "gas_pressure",
+            "material_id",
+            "sigma_xx",
+            "sigma_yy",
+            "sigma_zz",
+        },
+        "NodeReordering": {"input_mesh", "output_mesh", "no_volume_check"},
+        "OGS2VTK": {"ascii_output"},
+        "partmesh": {"exe_metis"},
+        "pvtu2vtu": {"original_mesh"},
+        "ResetPropertiesInPolygonalRegion": {"any_of"},
+        "vtkdiff": {"first_data_array", "second_data_array", "mesh_check"},
+        "xdmfdiff": {"first_data_array", "second_data_array", "mesh_check"},
+    }
+
     @staticmethod
-    def _format_kv(kwargs: Any) -> Any:
+    def _format_kv(kwargs: Any, underscore_args: Collection[str] = ()) -> Any:
         for key, v in kwargs.items():
             # Convert None to True
             if v is None:
@@ -72,19 +119,26 @@ class CLI_ON_PATH:
             if len(key) == 1:
                 yield f"-{key}"
             else:
-                yield f"--{key.replace('_', '-')}"
+                flag = key if key in underscore_args else key.replace("_", "-")
+                yield f"--{flag}"
 
             # Pass value if not bool
             if not isinstance(v, bool):
                 yield f"{v}"
 
     @staticmethod
-    def _get_cmdline(cmd: str, *args: Any, **kwargs: Any) -> list[str]:
-        str_kwargs = list(CLI_ON_PATH._format_kv(kwargs))
+    def _get_cmdline(
+        cmd: str,
+        *args: Any,
+        underscore_args: Collection[str] = (),
+        **kwargs: Any,
+    ) -> list[str]:
+        str_kwargs = list(CLI_ON_PATH._format_kv(kwargs, underscore_args))
         return [cmd] + str_kwargs + list(args)
 
     @staticmethod
     def _get_run_cmd(ogs_bin_dir: Path, attr: str) -> Callable:
+        underscore_args = CLI_ON_PATH._UNDERSCORE_ARGS.get(attr, set())
 
         def run_cmd(*args: Any, **kwargs: Any) -> int:
             cmd = str(ogs_bin_dir / attr)
@@ -96,7 +150,9 @@ class CLI_ON_PATH:
                 print("CMD:", cmd)
             if stderr is False:
                 stderr = subprocess.DEVNULL
-            cmdline = CLI_ON_PATH._get_cmdline(cmd, *args, **kwargs)
+            cmdline = CLI_ON_PATH._get_cmdline(
+                cmd, *args, underscore_args=underscore_args, **kwargs
+            )
             return subprocess.call(cmdline, stdout=stdout, stderr=stderr)
 
         # TODO: Only arguments with underscores work. Arguments with dashes not.
